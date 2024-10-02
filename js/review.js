b206d37fb7adc6ce548e2082c48dbf6c52415ae4
Julian <126jisaacs@frhsd.com>
2024-10-02 12:50:35 -0400

    hjhjkjk

---
+++ js/review.js
@@ -0,0 +1,26 @@
+console.log("This is External");
+console.log("Work on your typing skills");
+//Variables
+let variableName;
+const myName = "Julian";
+let lukeAge = 16;
+let tylerAge = 17;
+let kevinAge = 18;
+let mcdonaldAge = 33;
+let old = lukeAge + mcdonaldAge;
+console.log("Old Age =" + old);
+console.log(old + kevinAge);
+//Concatenation
+console.log('Super old is ${old +kevinAge}');
+//old = old +kevinAge;
+old += kevinAge;
+console.log(old);
+
+let lastName = "Isaacs"
+console.log(lastName);
+lastName = lastName + ", Julian";
+console.log(lastName.length);
+//Arithemitic
+console.log(10 % 2);
+console.log(14 % 2);
+console.log(35 % 2);
